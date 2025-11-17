68a988af1bf399b1c72b6f168b50526600998ad1
Duc Trung <nductrung205@gmail.com>
2025-11-17 15:50:17 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -2,23 +2,15 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => [
         'https://ban-do-an.vercel.app',
         'http://localhost:5173',
         'http://localhost:3000',
-        'https://food-api-xl8n.onrender.com',
     ],
-
-    'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
 ];
+
